c43bba8c8ba9fff442ade63537f41d91fa15e821
gabrielft-me <gabriel_fagundst@hotmail.com>
2026-03-02 23:39:35 -0500

    chore: add React + HTM helper

---
+++ src/lib/html.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useMemo, useRef, useState } from 'https://esm.sh/react@18.2.0';
+import htm from 'https://esm.sh/htm@3.1.1';
+
+const html = htm.bind(React.createElement);
+
+export { React, html, useEffect, useMemo, useRef, useState };
